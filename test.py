3fe2ed7251781e30191bbe5182ff6bd83e2f8475
Xev <email@email.com>
2020-01-28 17:33:51 +0000

    Changed print to python3 format

--- test.py
+++ test.py
@@ -1,3 +1,3 @@
 friends = ['john', 'pat', 'gary', 'michael']
-for i, name in enumerate(friends):
-    print ("iteration {iteration} is {name}".format(iteration=i, name=name))
+for iteration, name in enumerate(friends):
+    print(f"iteration {iteration} is {name}")
